8a2294af13989ca5ac99352c55c83cdb77f97dbf
p1ngul1n0 <57546870+p1ngul1n0@users.noreply.github.com>
2022-11-28 00:42:34 +0000

    Add files via upload

--- blackbird.py
+++ blackbird.py
@@ -27,7 +27,7 @@ async def findUsername(username, interfaceType):
     start_time = time.time()
     timeout = aiohttp.ClientTimeout(total=10)
 
-    print(f"{Fore.LIGHTYELLOW_EX}[!] Searching '{username}' across {len(searchData['sites'])} social networks\033[0m")
+    #print(f"{Fore.LIGHTYELLOW_EX}[!] Searching '{username}' across {len(searchData['sites'])} social networks\033[0m")
 
     async with aiohttp.ClientSession(timeout=timeout) as session:
         tasks = []
@@ -45,7 +45,7 @@ async def findUsername(username, interfaceType):
         # userFile = open(pathSave, 'w')
         # json.dump(userJson, userFile, indent=4, sort_keys=True)
 
-        print(f"{Fore.LIGHTYELLOW_EX}[!] Search complete in {executionTime} seconds\033[0m")
+        #print(f"{Fore.LIGHTYELLOW_EX}[!] Search complete in {executionTime} seconds\033[0m")
         # print(f"{Fore.LIGHTYELLOW_EX}[!] Results saved to {username}.json\033[0m")
         return userJson
 
@@ -72,58 +72,26 @@ async def makeRequest(session, u, username, interfaceType):
                 soup = BeautifulSoup(responseContent, 'html.parser')
 
             if eval(u["valid"]):
-                print(f'{Fore.LIGHTGREEN_EX}[+]\033[0m - #{u["id"]} {Fore.BLUE}{u["app"]}\033[0m {Fore.LIGHTGREEN_EX}account found\033[0m - {Fore.YELLOW}{url}\033[0m [{response.status} {response.reason}]\033[0m')
+                #print(f'{Fore.LIGHTGREEN_EX}[+]\033[0m - #{u["id"]} {Fore.BLUE}{u["app"]}\033[0m {Fore.LIGHTGREEN_EX}account found\033[0m - {Fore.YELLOW}{url}\033[0m [{response.status} {response.reason}]\033[0m')
                 if 'metadata' in u:
                     metadata = []
                     for d in u["metadata"]:
                         try:
                             value = eval(d['value']).strip('\t\r\n')
-                            print(f"   |--{d['key']}: {value}")
+                            #print(f"   |--{d['key']}: {value}")
                             metadata.append({"type": d["type"], "key": d['key'], "value": value})
                         except Exception as e:
                             pass
                 return ({"id": u["id"], "app": u['app'], "url": url, "response-status": f"{response.status} {response.reason}", "status": "FOUND", "error-message": None, "metadata": metadata})
             else:
-                if interfaceType == 'CLI':
-                    if showAll:
-                        print(f'[-]\033[0m - #{u["id"]} {Fore.BLUE}{u["app"]}\033[0m account not found - {Fore.YELLOW}{url}\033[0m [{response.status} {response.reason}]\033[0m')
                 return ({"id": u["id"], "app": u['app'], "url": url, "response-status": f"{response.status} {response.reason}", "status": "NOT FOUND", "error-message": None, "metadata": metadata})
     except Exception as e:
-        if interfaceType == 'CLI':
-            if showAll:
-                print(f'{Fore.RED}[X]\033[0m - #{u["id"]} {Fore.BLUE}{u["app"]}\033[0m error on request ({repr(e)})- {Fore.YELLOW}{url}\033[0m')
         return ({"id": u["id"], "app": u['app'], "url": url, "response-status": None, "status": "ERROR", "error-message": repr(e), "metadata": metadata})
 
 
 def list_sites():
     for i, u in enumerate(searchData["sites"], 1):
         print(f'{i}. {u["app"]}')
-
-
-def read_results(file):
-    try:
-        pathRead = os.path.join(path, 'results', file)
-        f = open(pathRead, 'r')
-        jsonD = json.load(f)
-        print(f'Loaded results file: {file}')
-        print(f"Username: {jsonD['search-params']['username']}")
-        print(f"Number of sites: {jsonD['search-params']['sites-number']}")
-        print(f"Date: {jsonD['search-params']['date']}")
-        print('-------------------------------------------------')
-        for u in jsonD['sites']:
-            if u['status'] == "FOUND":
-                print(f'{Fore.LIGHTGREEN_EX}[+]\033[0m - {Fore.BLUE}{u["app"]}\033[0m {Fore.LIGHTGREEN_EX}account found\033[0m - {Fore.YELLOW}{u["url"]}\033[0m [{u["response-status"]}]\033[0m')
-                if u["metadata"]:
-                    for d in u["metadata"]:
-                        print(f"   |--{d['key']}: {d['value']}")
-            elif u['status'] == "ERROR":
-                print(f'{Fore.RED}[X]\033[0m - {Fore.BLUE}{u["app"]}\033[0m error on request ({u["error-message"]}) - {Fore.YELLOW}{u["url"]}\033[0m')
-            elif u['status'] == "NOT FOUND":
-                print(f'{Fore.WHITE}[-]\033[0m - {Fore.BLUE}{u["app"]}\033[0m account not found - {Fore.YELLOW}{u["url"]}\033[0m [{u["response-status"]}]\033[0m')
-
-    except Exception as e:
-        print(f'{Fore.RED}[X] Error reading file [{repr(e)}]')
-
 
 if __name__ == '__main__':
     init()
