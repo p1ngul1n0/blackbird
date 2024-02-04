040f523eee2d6041ca951ce4f5c1aefa50aa7e40
p1ngul1n0 <57546870+p1ngul1n0@users.noreply.github.com>
2024-02-03 21:33:30 -0300

    Added update support

--- blackbirdv2.py
+++ blackbirdv2.py
@@ -21,22 +21,32 @@ def doSyncRequest(method, url):
     return response, parsedData
 
 
-def updateList():
+def downloadList():
     response, parsedData = doSyncRequest("GET", listURL)
     with open("wmn-data.json", "w", encoding="utf-8") as f:
         json.dump(parsedData, f, indent=4)
 
+def hashJSON(jsonData):
+    dumpJson = json.dumps(jsonData, sort_keys=True)
+    jsonHash = hashlib.md5(dumpJson.encode("utf-8")).hexdigest()
+    return jsonHash
 
 def checkUpdates():
     if os.path.isfile("wmn-data.json"):
         print("Checking for updates...")
         f = open("wmn-data.json")
         data = json.load(f)
-        hashMd5 = hashlib.md5(data.encode("utf-8")).hexdigest()
-        print(hashMd5)
+        currentListHash = hashJSON(data)
+        response, data = doSyncRequest("GET", listURL)
+        remoteListHash = hashJSON(data)
+        if (currentListHash != remoteListHash):
+            print("Updating...")
+            downloadList()
+        else:
+            print("[+] List is up to date")
     else:
         print("Downloading WhatsMyName list")
-        updateList()
+        downloadList()
 
 
 if __name__ == "__main__":
