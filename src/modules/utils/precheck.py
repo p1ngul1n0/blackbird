989b218d79b8962a57b6358b0c489c54aec1c1f8
p1ngul1n0 <57546870+p1ngul1n0@users.noreply.github.com>
2024-05-31 16:48:56 -0300

    Issue 76 fix
    Fixed metadata error

--- src/modules/utils/precheck.py
+++ src/modules/utils/precheck.py
@@ -1,4 +1,5 @@
 from modules.utils.http_client import do_sync_request
+import config
 
 
 def perform_pre_check(precheck_params, headers):
@@ -10,9 +11,15 @@ def perform_pre_check(precheck_params, headers):
     if precheck_params["type"] == "cookie":
         cookie_name = precheck_params["cookie_name"]
         cookie_value = response.cookies.get(precheck_params["cookie_name"])
-        for header in headers:
-            headers[header] = headers[header].replace(
-                "{" + cookie_name + "_value}", cookie_value
-            )
+        print(cookie_name)
+        if cookie_value:
+            if config.verbose:
+                config.console.print(
+                    f"🔑 Acquired cookie {cookie_name}: {cookie_value}"
+                )
+            for header in headers:
+                headers[header] = headers[header].replace(
+                    "{" + cookie_name + "_value}", cookie_value
+                )
 
     return headers
