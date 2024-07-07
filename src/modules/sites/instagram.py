5ba6c2c061872760964941979bc432ab25dbdb9b
p1ngul1n0 <57546870+p1ngul1n0@users.noreply.github.com>
2024-07-06 22:18:47 -0300

    Instagram Metadata extraction

---
+++ src/modules/sites/instagram.py
@@ -0,0 +1,207 @@
+from modules.utils.http_client import do_sync_request
+from modules.utils.log import logError
+from modules.utils.parse import extractMetadata
+from json import dumps
+from urllib.parse import quote_plus
+import config
+
+metadataParams = [
+    {
+        "schema": "JSON",
+        "type": "Image",
+        "name": "Profile Picture URL",
+        "path": ["user", "profile_pic_url"],
+    },
+    {"schema": "JSON", "type": "String", "name": "User ID", "path": ["user", "pk_id"]},
+    {
+        "schema": "JSON",
+        "type": "String",
+        "name": "Full Name",
+        "path": ["user", "full_name"],
+    },
+    {
+        "schema": "JSON",
+        "type": "String",
+        "name": "Biography",
+        "path": ["user", "biography"],
+    },
+    {
+        "schema": "JSON",
+        "type": "String",
+        "name": "Follower Count",
+        "path": ["user", "follower_count"],
+    },
+    {
+        "schema": "JSON",
+        "type": "String",
+        "name": "Following Count",
+        "path": ["user", "following_count"],
+    },
+    {
+        "schema": "JSON",
+        "type": "String",
+        "name": "External URL",
+        "path": ["user", "external_url"],
+    },
+    {
+        "schema": "JSON",
+        "type": "String",
+        "name": "Category",
+        "path": ["user", "category"],
+    },
+    {
+        "schema": "JSON",
+        "type": "String",
+        "name": "Is Verified",
+        "path": ["user", "is_verified"],
+    },
+]
+
+metadataParams2 = [
+    {"schema": "JSON", "type": "String", "name": "Email Sent", "path": ["email_sent"]},
+    {"schema": "JSON", "type": "String", "name": "SMS Sent", "path": ["sms_sent"]},
+    {"schema": "JSON", "type": "String", "name": "WhatsApp Sent", "path": ["wa_sent"]},
+    {
+        "schema": "JSON",
+        "type": "String",
+        "name": "Lookup Source",
+        "path": ["lookup_source"],
+    },
+    {
+        "schema": "JSON",
+        "type": "String",
+        "name": "Corrected Input",
+        "path": ["corrected_input"],
+    },
+    {
+        "schema": "JSON",
+        "type": "String",
+        "name": "Obfuscated Email",
+        "path": ["obfuscated_email"],
+    },
+    {
+        "schema": "JSON",
+        "type": "String",
+        "name": "Obfuscated Phone",
+        "path": ["obfuscated_phone"],
+    },
+    {
+        "schema": "JSON",
+        "type": "String",
+        "name": "Is Private",
+        "path": ["user", "is_private"],
+    },
+    {
+        "schema": "JSON",
+        "type": "String",
+        "name": "Has Valid Phone",
+        "path": ["has_valid_phone"],
+    },
+    {
+        "schema": "JSON",
+        "type": "String",
+        "name": "Can Email Reset",
+        "path": ["can_email_reset"],
+    },
+    {
+        "schema": "JSON",
+        "type": "String",
+        "name": "Can SMS Reset",
+        "path": ["can_sms_reset"],
+    },
+    {
+        "schema": "JSON",
+        "type": "String",
+        "name": "Can WhatsApp Reset",
+        "path": ["can_wa_reset"],
+    },
+    {
+        "schema": "JSON",
+        "type": "String",
+        "name": "Facebook Login Option",
+        "path": ["fb_login_option"],
+    },
+    {"schema": "JSON", "type": "String", "name": "Status", "path": ["status"]},
+]
+
+
+def get_user_id(username, session_id):
+    try:
+        headers = {"User-Agent": "iphone_ua", "x-ig-app-id": "936619743392459"}
+        cookies = {"sessionid": session_id}
+        response = do_sync_request(
+            method="GET",
+            url=f"https://i.instagram.com/api/v1/users/web_profile_info/?username={username}",
+            data=None,
+            customHeaders=headers,
+            cookies=cookies,
+        )
+        user_id = response.json()["data"]["user"]["id"]
+        if config.verbose:
+            config.console.print(f"[Instagram] Acquired {username} user ID")
+        return user_id
+
+    except Exception as e:
+        logError(e, f"[Instagram] Coudn't acquire {username} user ID")
+        return False
+
+
+def get_instagram_account_info(username, session_id):
+    extractedMetadata = []
+    try:
+        user_id = get_user_id(username, session_id)
+
+        if user_id:
+
+            headers = {"User-Agent": "Instagram 64.0.0.14.96"}
+            cookies = {"sessionid": session_id}
+            url = f"https://i.instagram.com/api/v1/users/{user_id}/info/"
+            response = do_sync_request(
+                method="GET",
+                url=url,
+                data=None,
+                customHeaders=headers,
+                cookies=cookies,
+            )
+            data = response.json()
+            response = {"json": data}
+
+            if data:
+                metadata = extractMetadata(
+                    metadata=metadataParams, response=response, site="Instagram"
+                )
+                extractedMetadata.extend(metadata)
+
+                data = "signed_body=SIGNATURE." + quote_plus(
+                    dumps({"q": username, "skip_recovery": "1"}, separators=(",", ":"))
+                )
+                headers = {
+                    "Accept-Language": "en-US",
+                    "User-Agent": "Instagram 101.0.0.15.120",
+                    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                    "X-IG-App-ID": "124024574287414",
+                    "Accept-Encoding": "gzip, deflate",
+                    "Host": "i.instagram.com",
+                    "Connection": "keep-alive",
+                    "Content-Length": str(len(data)),
+                }
+
+                response = do_sync_request(
+                    method="POST",
+                    url="https://i.instagram.com/api/v1/users/lookup/",
+                    data=data,
+                    customHeaders=headers,
+                )
+                data = response.json()
+                response = {"json": data}
+
+                if data:
+                    metadata = extractMetadata(
+                        metadata=metadataParams2, response=response, site="Instagram"
+                    )
+                    extractedMetadata.extend(metadata)
+
+                return extractedMetadata
+    except Exception as e:
+        logError(e, f"[Instagram] Coudn't acquire more metadata")
+        return False
